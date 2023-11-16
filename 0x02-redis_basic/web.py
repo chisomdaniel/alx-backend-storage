c78ccc9bd32665b9bca55d79867452f837339530
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-16 09:31:06 +0100

    advanced

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''implement an expiring web cache and tracker'''
+import requests
+from functools import wraps
+from typing import Callable
+import redis
+
+
+def count_access(fn: Callable) -> Callable:
+    '''count how many times a url was accessed'''
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        '''wrapper function'''
+        key = "count:{}".format(url)
+        r = redis.Redis()
+        count = r.get(key)
+        if count is None:
+            count = b'0'
+        count = int(count.decode())
+        r.setex(key, 10, value=count+1)
+        return fn(url)
+    return wrapper
+
+
+@count_access
+def get_page(url: str) -> str:
+    '''get a page and return the content'''
+    r = requests.get(url)
+    return r.content
